78a3f1c973489c9dfd5a308b3ce5a97613f1bac7
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-22 17:22:18 -0400

    Add untested OSM record normalization.
    index.js
      -Define `createOSMStream()`, to create a readable stream of OSM
      records.
      -Define `normalizer()`, a normalization filter.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello, world.');
+'use strict';
+
+var fs = require( 'fs' );
+var JSONStream = require( 'JSONStream');
+var through = require( 'through' );
+
+function createOSMStream( path ) {
+  var file = path;
+  return fs.createReadStream( path )
+    .pipe( JSONStream.parse( 'features.*' ) );
+}
+
+var normalizer = through( function write(record){
+  if(record[ 'geometry' ][ 'type' ] === 'Point' ||
+    record[ 'properties' ][ 'addr:street' ] === null){
+    return;
+  }
+
+  function emptyIfNull(string){
+    return ( string === null ) ? '' : string;
+  }
+
+  this.push({
+    "house" : emptyIfNull( record[ 'properties' ][ 'addr:housename' ] ) +
+      emptyIfNull( record[ 'properties' ][ 'addr:housenumber' ] ),
+    "street" : record[ 'properties' ][ 'addr:street' ],
+    "city" : record[ 'properties' ][ 'addr:city' ],
+    "state" : record[ 'properties' ][ 'addr:district' ],
+    "zip" : record[ 'properties' ][ 'addr:postcode' ]
+  });
+});
+
+createOSMStream( 'tmp.json' )
+  .pipe( normalizer )
+  .pipe( JSONStream.stringify() )
+  .pipe( process.stdout );
