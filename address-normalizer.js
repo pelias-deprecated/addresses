da795a1c141d5bf35b53abcf14eb7558027013a8
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-23 13:15:03 -0400

    Define and document an Address constructor.
    address-normalizer.js
      -Define/document `Address()`, a constructor for address objects.
      -Call `Address()` from the module's exported `through` stream,
      instead of declaring the queued address as an on-the-fly object.

--- address-normalizer.js
+++ address-normalizer.js
@@ -7,21 +7,28 @@
 var through = require( 'through' );
 
 /**
+ * Create a normalized address object.
+ *
+ * @param {string} house The unique identifier of the house/building.
+ * @param {string} street The street name.
+ * @param {string} city The city name.
+ * @param {string} state The region/district/state name.
+ * @param {string} zip The zip-code.
+ */
+function Address(house, street, city, state, zip){
+  this.house = house;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
+
+/**
  * Filter out and normalize OSM records.
  *
  * Creates a `through` stream that expects a stream of OSM records. Filters out
  * records that aren't Points or don't have street names, and normalizes the
- * others into the following format:
- *
- *      {
- *          'house' : ...,
- *          'street' : ...,
- *          'city' : ...,
- *          'state' : ...,
- *          'zip' : ...
- *      }
- *
- * Only `street` is guaranteed to be non-null.
+ * others into an Address object.
  */
 module.exports = through( function write(record){
   if(record.geometry.type !== 'Point' ||
@@ -33,12 +40,12 @@ module.exports = through( function write(record){
     return ( string === null ) ? '' : string;
   }
 
-  this.push({
-    'house' : emptyIfNull( record.properties[ 'addr:housename']  ) +
+  this.push(new Address(
+    emptyIfNull( record.properties[ 'addr:housename']  ) +
       emptyIfNull( record.properties[ 'addr:housenumber' ] ),
-    'street' : record.properties[ 'addr:street' ],
-    'city' : record.properties[ 'addr:city' ],
-    'state' : record.properties[ 'addr:district' ],
-    'zip' : record.properties[ 'addr:postcode' ]
-  });
+    record.properties[ 'addr:street' ],
+    record.properties[ 'addr:city' ],
+    record.properties[ 'addr:district' ],
+    record.properties[ 'addr:postcode' ]
+  ));
 });
