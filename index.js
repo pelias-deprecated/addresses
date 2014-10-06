35c40e6756b21fc7ae1d867ee86329e9b6746447
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-06 14:59:39 -0400

    Remove handleUserInput(). Adapt to new osm/tiger.
    index.js
      -Remove the obsolete `handleUserInput()`, used for testing
      `lib/addresses/osm.js` easily via command-line arguments.

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
 'use strict';
 
 var fs = require( 'fs' );
-var osmAddresses = require( './lib/addresses/osm' );
-var tigerAddresses = require( './lib/addresses/tiger' );
+var osm = require( './lib/addresses/osm' );
+var tiger = require( './lib/addresses/tiger' );
 var util = require( 'util' );
-
-/**
- * Handle user arguments.
- *
- * @param {array of strings} Command line arguments (`process.argv`).
- */
-function handleUserInput( argv ){
-  var useMessage = [
-    'Insufficient number of arguments.',
-    'use: node index.js [ --help | --stdin | FILENAME ]',
-    '\n\t--help : print this message and exit.',
-    '\t--stdin : read a PBF from stdin.',
-    '\tFILENAME : the name of a PBF file to read.'
-  ].join( '\n' );
-
-  if( argv.length !== 3 ){
-    console.error( useMessage );
-    process.exit( 1 );
-  }
-
-  else {
-    var pbfStream;
-
-    switch( argv[ 2 ] ){
-      case '--help':
-        console.log( useMessage );
-        return;
-      case '--stdin':
-        pbfStream = process.stdin;
-        break;
-      default:
-        pbfStream = fs.createReadStream( argv[ 2 ] );
-        break;
-    }
-
-    var pipeline = require( 'through' )( function write( obj ){
-      console.log( JSON.stringify( obj, undefined, 2 ) );
-    }); // temporary: for debugging
-    osmAddresses( pbfStream ).pipe( pipeline );
-  }
-}
 
 function testImports(){
   var testDir = 'test_sources/';
@@ -64,8 +23,11 @@ function testImports(){
   var addressesPipeline = require( 'through' )( function write( obj ){
     console.log( JSON.stringify( obj, undefined, 2 ) );
   });
-  // osmAddresses( fs.createReadStream( osmPath ) ).pipe( addressesPipeline );
-  tigerAddresses( tigerPath ).pipe( addressesPipeline );
+  // osm.addressStream( fs.createReadStream( osmPath ) )
+    // .pipe( addressesPipeline );
+
+  tiger.addressStream( tigerPath )
+    .pipe( addressesPipeline );
 }
 
 testImports();
