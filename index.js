ae189c6d75e21bf0f2eca8c3d8001fea7b117f7d
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-07 15:36:42 -0400

    Complete handleUserArgs().
    index.js
      -Define `createFileAddressStreams()`, for use in `handleUserArgs()`.
      -Create combined address streams for specified OSM/TIGER data.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// 'use strict';
+'use strict';
 
 var fs = require( 'fs' );
 var path = require( 'path' );
-var util = require( 'util' );
 
 var CombinedStream = require('combined-stream');
 var minimist = require( 'minimist' );
 var osm = require( './lib/addresses/osm' );
 var tiger = require( './lib/addresses/tiger' );
+
+function createFileAddressStreams( dirName, filenameRegex, action ){
+  var files = fs.readdirSync( dirName );
+  for( var file = 0; file < files.length; file++ ){
+    var filePath = path.join( dirName, files[ file ] );
+    if( ( fs.lstatSync( filePath ).isFile() &&
+      filenameRegex.test( filePath ) ) ){
+      action( filePath );
+    }
+  }
+}
 
 function handleUserArgs( rawArgs ){
   var helpMessage = [
@@ -29,6 +39,35 @@ function handleUserArgs( rawArgs ){
     return;
   }
   else {
+    var addressStream = CombinedStream.create();
+
+    if( args.hasOwnProperty( 'osm' ) ){
+      createFileAddressStreams(
+        args.osm,
+        /\.osm\.pbf$/,
+        function ( filename ){
+          var osmStream = osm.addressStream(
+            fs.createReadStream( filename )
+          );
+          addressStream.append( osmStream );
+        }
+      );
+    }
+
+    if( args.hasOwnProperty( 'tiger' ) ){
+      createFileAddressStreams(
+        args.tiger,
+        /\.shp$/,
+        function ( filename ){
+          addressStream.append( tiger.addressStream( filename ) );
+        }
+      );
+    }
+
+    var addressesPipeline = require( 'through' )( function write( obj ){
+      console.log( JSON.stringify( obj.house, undefined, 2 ) );
+    });
+    addressStream.pipe( addressesPipeline );
   }
 }
 
