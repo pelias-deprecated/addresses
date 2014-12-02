8a00593590daa750171cecfa73c740e61714ca2d
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-02 14:16:06 -0500

    Refactor the test/run module.
    test/run.js
    	-Rewrite/refactor the `test/run` module using `schema-mapper`'s
    	test structure as a model. Writing tests is now a little more
    	lightweight than it was previously, since multiple superfluous
    	variables/arguments have been discarded.

--- test/run.js
+++ test/run.js
@@ -1,25 +1,47 @@
+/**
+ * @file The main entry point for the address pipeline's unit tests.
+ *      Imports and runs all of the sibling files ( assumed to be modules that
+ *      export objects containing `tape` test functions ) in this file's
+ *      directory.
+ */
+
 'use strict';
 
+var fs = require( 'fs' );
 var util = require( 'util' );
 var tape = require( 'tape' );
-var common = {};
 
-var testPaths = [
-  'osm',
-  'tiger',
-  'openaddresses'
-];
-
-testPaths.map( function( path ) {
-  function runTest( name, testFunction ){
-    return tape( util.format( '%s: %s', path, name ), testFunction );
-  }
-
-  var testModule = require( './' + path );
-
-  for( var testCase in testModule.tests ){
-    if( testModule.tests.hasOwnProperty( testCase ) ){
-      testModule.tests[ testCase ]( runTest, common );
+/**
+ * Execute all functions in the object exported by a module.
+ *
+ * @param {string} testFilePath The path to the module. Will be imported, and
+ *      each of its exported functions will be executed by `tape()`.
+ */
+function runTestFile( testFilePath ){
+  var testModule = require( testFilePath );
+  for( var test in testModule ){
+    if( testModule.hasOwnProperty( test ) ){
+      var testName = util.format(
+        'File: %s, function: %s.', testFilePath, test
+       );
+      tape( testName, testModule[ test ] );
     }
   }
-});
+}
+
+/**
+ * Run all modules in this module's directory with `runTestFile()`.
+ */
+!function runTestFiles(){
+  fs.readdir( __dirname, function readFilesCallback( err, files ){
+    if( err ){
+      console.err( err );
+      process.exit( 1 );
+    }
+
+    files.splice( files.indexOf( __filename ) );
+    for( var ind = 0; ind < files.length; ind++ ){
+      runTestFile( './' + files[ ind ] );
+    }
+  } )
+}();
